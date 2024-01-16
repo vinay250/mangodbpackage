2111ba8fb8fca59625ac2ae35e736bb739014554
“vinayaka <vinayakavirat008@gmail.com>
2024-01-16 11:34:32 +0530

    commit

--- setup.py
+++ setup.py
@@ -1,6 +1,19 @@
 from setuptools import setup, find_packages
 from typing import List
 
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
@@ -25,4 +38,5 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
+    install_requires=get_requirement("./requirements_dev.txt"),
     )
