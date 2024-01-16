5a2361fec0ba87fad8568b9bc524fe6f1fd6d100
“vinayaka <vinayakavirat008@gmail.com>
2024-01-16 13:49:23 +0530

    commit

--- setup.py
+++ setup.py
@@ -1,22 +1,12 @@
 from setuptools import setup, find_packages
 from typing import List
 
-"""HYPEN_E_DOT='-e .'
-
-def get_requirements(file_path:str)->List[str]:
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
-
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-
-    return requirements"""
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()     
+   
     
 
-with open(r'E:\Mangodpackage\README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()
+
 
 
 
@@ -31,14 +21,14 @@ setup(
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A python package for connecting with the database.",
+    description="A python package for connecting with database.",
     long_description=long_description,
-    long_description_content_type="text/markdown",  # Corrected parameter name
+    long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-
     )
+    
