68cb3d02b6011903d24f8890c65450b1747c30e1
“vinayaka <vinayakavirat008@gmail.com>
2024-01-16 15:00:29 +0530

    commit

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
+from typing import List
 
 with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()
+    long_description = f.read()     
 
 __version__ = "0.0.4"
 REPO_NAME = "mangodbpackage"
@@ -14,13 +15,13 @@ setup(
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A python package for connecting with a database.",
+    description="A python package for connecting with database.",
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-)
+    )
